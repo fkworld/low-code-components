e49f3b18a11dc3b44f4e47b8be2c9fbee81ded55
fkworld <fkworld@foxmail.com>
2025-06-16 02:52:03 +0000

    feat(components): 新增 NodeInputText 文档

---
+++ apps/docs/src/node/node-input/node-input-text.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Node } from "components";
+
+const meta: Meta<typeof Node> = {
+  title: "Node-Input/NodeInputText",
+  component: Node,
+  argTypes: {
+    type: {
+      control: false,
+    },
+  },
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Node> = {
+  args: {
+    type: "inputText",
+    value: "NodeInputText",
+  },
+};
